b889dc2ffa8e5fc5286c4b1f1f1a473bb7344087
Jonathan <jonathandrewkerr@gmail.com>
2019-10-03 16:27:20 +0100

    Build basics of Candidate from intake spreadsheet
    The data needed for this project is held across two CSV files per intake, multipled by two CSV files per scheme. There are some differences in each file. This first commit extracts data solely from the intake file, which is different to the applications file. The next commit will bring in the applications file and refactor the current classes, drawing out commonalities and synthesising a single perfect record. Hopefully

--- modules/upload.py
+++ modules/upload.py
@@ -1,47 +1,125 @@
 import csv
-from app.models import Candidate, Role, Scheme, Grade, Gender, Ethnicity, Sexuality, Organisation, Profession, db, \
-    Location, Promotion, Application
-import datetime
+from app.models import (
+    Candidate,
+    Role,
+    Scheme,
+    Grade,
+    Gender,
+    Ethnicity,
+    Sexuality,
+    Organisation,
+    Profession,
+    db,
+    Location,
+    Promotion,
+    Application,
+)
+from datetime import datetime
+from typing import List
 
 
 class Upload:
-    def __init__(self, filepath: str, programme: str, scheme_start_date: str):
-        with open(filepath, 'r') as f:
-            self.file_content = list(csv.DictReader(f))
+    def __init__(self, intake_filepath: str, programme: str, scheme_start_date: str):
+        with open(intake_filepath, "r") as f:
+            self.intake_file_content = list(csv.DictReader(f))
         self.scheme: Scheme = Scheme.query.filter_by(name=programme).first()
-        self.scheme_start_date = scheme_start_date
-
-    def long_term_health_condition_translation(self, response: str):
-        return True if response == 'Yes' else False
-
-    def create_candidate_data_row(self, row: dict):
-        c = Candidate(
-            first_name=row.get('First Name'), last_name=row.get('Last Name'), email_address=row.get('Email Address'),
-            sexuality=Sexuality.query.filter_by(value=row.get('Sexual Orientation')).first(),
-            long_term_health_condition=self.long_term_health_condition_translation(row.get('Disability')),
-            gender=Gender.query.filter_by(value=row.get('Gender')).first(),
-            joining_grade=Grade.query.filter_by(value=row.get('Original Grade')).first(),
-            ethnicity=Ethnicity.query.filter_by(value=row.get('Ethnicity')).first(),
-            roles=[
-                Role(
-                    date_started=datetime.date(2019, 1, 1), role_name=row.get('Job Title'),
-                    organisation=Organisation.query.filter_by(name=row.get('Department')).first(),
-                    profession=Profession.query.filter_by(value=row.get('Profession')),
-                    location=Location.query.filter_by(value=row.get('Location')),
-                    grade=Grade.query.filter_by(value=row.get('Current Grade')).first(),
-                    role_change=Promotion.query.filter_by(value='substantive')
-                )
-            ],
-            applications=[
-                Application(
-                    scheme_id=self.scheme.id, scheme_start_date=self.scheme_start_date, successful=True,
-                    meta=row.get('META'), delta=row.get('DELTA'), cohort=row.get('Cohort')
-                )
-            ]
-        )
-        return c
+        self.scheme_start_date = datetime.strptime(scheme_start_date, "%Y-%m-%d").date()
 
     def complete_upload(self):
-        for row in self.file_content:
-            db.session.add(self.create_candidate_data_row(row))
+        for row in self.intake_file_content:
+            row = IntakeRow(row, self.scheme_start_date, self.scheme)
+            db.session.add(row.get_candidate())
         db.session.commit()
+
+
+class IntakeRow:
+    def __init__(self, row: dict, scheme_start_date: datetime.date, scheme: Scheme):
+        self.data = row
+        self.candidate = self._create_candidate_data()
+        self.scheme_start_date = scheme_start_date
+        self.scheme = scheme
+
+    def get_candidate(self):
+        self._process_row()
+        return self.candidate
+
+    @staticmethod
+    def _long_term_health_condition_translation(response: str) -> bool:
+        return True if response == "Yes" else False
+
+    @staticmethod
+    def _empty_translator(cell: str) -> bool:
+        return False if cell == "" else True
+
+    @staticmethod
+    def _separate_email_addresses(cell_content: str) -> List[str]:
+        for delim in ",;":
+            cell_content = cell_content.replace(delim, " ")
+        addresses = [address.strip() for address in cell_content.split()]
+        return addresses
+
+    def _process_row(self):
+        self._add_application()
+        self._add_most_recent_role()
+
+    def _get_title_or_not_provided(self):
+        title = self.data.get("Job Title")
+        return "Not provided" if title == "" else title
+
+    def _add_most_recent_role(self):
+        self.candidate.roles.append(
+            Role(
+                role_name=self._get_title_or_not_provided(),
+                organisation_id=Organisation.query.filter_by(
+                    name=self.data.get("Department")
+                )
+                .first()
+                .id,
+                profession=Profession.query.filter_by(
+                    value=self.data.get("Profession")
+                ).first(),
+                grade=Grade.query.filter_by(
+                    value=self.data.get("Current Grade")
+                ).first(),
+                location=Location.query.filter_by(
+                    value=self.data.get("Location")
+                ).first(),
+                role_change=Promotion.query.filter_by(value="substantive").first(),
+            )
+        )
+
+    def _create_candidate_data(self):
+        c = Candidate(
+            first_name=self.data.get("First Name"),
+            last_name=self.data.get("Last Name"),
+            sexuality=Sexuality.query.filter_by(
+                value=self.data.get("Sexual Orientation")
+            ).first(),
+            long_term_health_condition=self._long_term_health_condition_translation(
+                self.data.get("Disability")
+            ),
+            gender=Gender.query.filter_by(value=self.data.get("Gender")).first(),
+            joining_grade=Grade.query.filter_by(
+                value=self.data.get("Original Grade")
+            ).first(),
+            ethnicity=Ethnicity.query.filter_by(
+                value=self.data.get("Ethnicity")
+            ).first(),
+        )
+        addresses = self._separate_email_addresses(self.data.get("Email Address"))
+        c.email_address = addresses[0]
+        if len(addresses) > 1:
+            c.secondary_email_address = addresses[1]
+        return c
+
+    def _add_application(self):
+        self.candidate.applications.append(
+            Application(
+                scheme_id=self.scheme.id,
+                scheme_start_date=self.scheme_start_date,
+                successful=True,
+                meta=self._empty_translator(self.data.get("META", False)),
+                delta=self._empty_translator(self.data.get("DELTA", False)),
+                cohort=self.data.get("Cohort"),
+            )
+        )
--- tests/test_upload.py
+++ tests/test_upload.py
@@ -1,11 +1,17 @@
-from modules import upload
+from modules.upload import Upload
 import os
+from app.models import Candidate, Organisation
+
 
 class TestUpload:
-
-    def test_create_candidate_data(self):
+    def test_create_candidate_data(self, test_session):
+        test_session.add(Organisation(name="Foreign and Commonwealth Office"))
+        test_session.commit()
         directory = os.path.dirname(__file__)
-        filename = os.path.join(directory, 'data/test_csv.csv')
-        u = upload.Upload(filename, 'FLS', '3/3/2020')
-        c = u.create_candidate_data_row(u.file_content[0])
-        assert c
+        filename = os.path.join(directory, "data/test_csv.csv")
+        u = Upload(filename, "FLS", "2020-3-3")
+        u.complete_upload()
+        candidate = Candidate.query.filter_by(first_name="James").first()
+        assert candidate
+        assert len(candidate.applications.all()) == 1
+        assert len(candidate.roles.all()) == 1
