df6cf5041e6f96367126109559a348c78db08398
Jonathan <jonathandrewkerr@gmail.com>
2019-10-03 20:06:58 +0100

    Refactor the Upload class to use pandas
    I've done a really significant refactoring of the Upload class and renamed the IntakeRow class to Row. This is because I've been able to synthesise the two CSV files using pandas and joined them together.
    
    The tests aren't get finished - I need to fully assert all the attributes of the Candidate I'm testing - but I'm confident they'll work.
    
    As I always am when they're not tested.

--- modules/upload.py
+++ modules/upload.py
@@ -1,42 +1,49 @@
-import csv
-from app.models import (
-    Candidate,
-    Role,
-    Scheme,
-    Grade,
-    Gender,
-    Ethnicity,
-    Sexuality,
-    Organisation,
-    Profession,
-    db,
-    Location,
-    Promotion,
-    Application,
-)
-from datetime import datetime
+from app.models import *
+from datetime import datetime, date
 from typing import List
+import pandas as pd
 
 
 class Upload:
-    def __init__(self, intake_filepath: str, programme: str, scheme_start_date: str):
-        with open(intake_filepath, "r") as f:
-            self.intake_file_content = list(csv.DictReader(f))
+    def __init__(
+        self,
+        intake_filepath: str,
+        programme: str,
+        scheme_start_date: str,
+        application_filepath: str,
+    ):
+        self.intake_filepath = intake_filepath
+        self.application_filepath = application_filepath
         self.scheme: Scheme = Scheme.query.filter_by(name=programme).first()
         self.scheme_start_date = datetime.strptime(scheme_start_date, "%Y-%m-%d").date()
+        self.intake_dataframe = pd.read_csv(self.intake_filepath)
+        self.application_dataframe = pd.read_csv(self.application_filepath)
+        self.joined_dataframe: pd.DataFrame = self.join_csvs()
 
     def complete_upload(self):
-        for row in self.intake_file_content:
-            row = IntakeRow(row, self.scheme_start_date, self.scheme)
+        for i in range(len(self.joined_dataframe.index)):
+            row_series = self.joined_dataframe.iloc[i]
+            row = Row(row_series, self.scheme_start_date, self.scheme)
             db.session.add(row.get_candidate())
         db.session.commit()
 
+    def join_csvs(self):
+        return pd.merge(
+            self.intake_dataframe,
+            self.application_dataframe,
+            left_on="Psych. Username",
+            right_on="PerID",
+            how="left",
+        )
 
-class IntakeRow:
-    def __init__(self, row: dict, scheme_start_date: datetime.date, scheme: Scheme):
+
+class Row:
+    def __init__(
+        self, row: pd.Series, scheme_start_date: datetime.date, scheme: Scheme
+    ):
         self.data = row
         self.candidate = self._create_candidate_data()
-        self.scheme_start_date = scheme_start_date
+        self.scheme_start_date: date = scheme_start_date
         self.scheme = scheme
 
     def get_candidate(self):
@@ -44,7 +51,7 @@ class IntakeRow:
         return self.candidate
 
     @staticmethod
-    def _long_term_health_condition_translation(response: str) -> bool:
+    def _yes_is_true_no_is_false_translator(response: str) -> bool:
         return True if response == "Yes" else False
 
     @staticmethod
@@ -61,6 +68,7 @@ class IntakeRow:
     def _process_row(self):
         self._add_application()
         self._add_most_recent_role()
+        self._add_first_role()
 
     def _get_title_or_not_provided(self):
         title = self.data.get("Job Title")
@@ -71,42 +79,70 @@ class IntakeRow:
             Role(
                 role_name=self._get_title_or_not_provided(),
                 organisation_id=Organisation.query.filter_by(
-                    name=self.data.get("Department")
+                    name=self.data.Department_x
                 )
                 .first()
                 .id,
                 profession=Profession.query.filter_by(
-                    value=self.data.get("Profession")
+                    value=self.data.Profession_x
                 ).first(),
                 grade=Grade.query.filter_by(
-                    value=self.data.get("Current Grade")
+                    value=self.data.get("Current Grade")[0:7]
                 ).first(),
-                location=Location.query.filter_by(
-                    value=self.data.get("Location")
-                ).first(),
+                location=Location.query.filter_by(value=self.data.Location_x).first(),
+                role_change=Promotion.query.filter_by(value="substantive").first(),
+                date_started=date(self.scheme_start_date.year - 1, 1, 1),
+            )
+        )
+
+    def _add_first_role(self):
+        self.candidate.roles.append(
+            Role(
+                date_started=self.candidate.joining_date,
+                role_name="Not given",
+                grade=self.candidate.joining_grade,
                 role_change=Promotion.query.filter_by(value="substantive").first(),
             )
         )
 
     def _create_candidate_data(self):
         c = Candidate(
-            first_name=self.data.get("First Name"),
-            last_name=self.data.get("Last Name"),
-            sexuality=Sexuality.query.filter_by(
-                value=self.data.get("Sexual Orientation")
-            ).first(),
-            long_term_health_condition=self._long_term_health_condition_translation(
-                self.data.get("Disability")
+            joining_date=date(self.data.get("CS Joining Year"), 1, 1),
+            completed_fast_stream=self._yes_is_true_no_is_false_translator(
+                self.data["Have you completed  Fast Stream?"]
             ),
-            gender=Gender.query.filter_by(value=self.data.get("Gender")).first(),
-            joining_grade=Grade.query.filter_by(
-                value=self.data.get("Original Grade")
+            first_name=self.data.get("First Name"),
+            last_name=self.data.get("Last Name_x"),
+            caring_responsibility=self._yes_is_true_no_is_false_translator(
+                self.data.get("Caring Responsibility")
+            ),
+            long_term_health_condition=self._yes_is_true_no_is_false_translator(
+                self.data.Disabled_x
+            ),
+            age_range=AgeRange.query.filter_by(
+                value=self.data.get("Age Group")
             ).first(),
-            ethnicity=Ethnicity.query.filter_by(
-                value=self.data.get("Ethnicity")
+            working_pattern=WorkingPattern.query.filter_by(
+                value=self.data.get("Working Pattern")
+            ).first(),
+            belief=Belief.query.filter_by(
+                value=self.data.get("Religion/Belief")
+            ).first(),
+            sexuality=Sexuality.query.filter_by(
+                value=self.data.get("Sexual Orientation_y")
+            ).first(),
+            ethnicity=Ethnicity.query.filter_by(value=self.data.Ethnicity_y).first(),
+            main_job_type=MainJobType.query.filter_by(
+                value=self.data.get(
+                    "Describes the sort of work the main/ highest income earner in your household did in their main job?"
+                )
+            ).first(),
+            gender=Gender.query.filter_by(value=self.data.Gender_x).first(),
+            joining_grade=Grade.query.filter_by(
+                value=self.data.get("CS Joining Grade")
             ).first(),
         )
-        addresses = self._separate_email_addresses(self.data.get("Email Address"))
+        addresses = self._separate_email_addresses(self.data.get("Email Address_y"))
         c.email_address = addresses[0]
         if len(addresses) > 1:
             c.secondary_email_address = addresses[1]
@@ -120,6 +156,6 @@ class IntakeRow:
                 successful=True,
                 meta=self._empty_translator(self.data.get("META", False)),
                 delta=self._empty_translator(self.data.get("DELTA", False)),
-                cohort=self.data.get("Cohort"),
+                cohort=self.data.Cohort,
             )
         )
